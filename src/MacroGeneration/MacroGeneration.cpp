6b3b143a6747895e3cb5b3638631d6e0057b02d0
Dovy <djasulaitis05@gmail.com>
2022-10-28 13:27:23 +0200

    fix crash ☠️☠️☠️☠️

--- src/MacroGeneration/MacroGeneration.cpp
+++ src/MacroGeneration/MacroGeneration.cpp
@@ -10,7 +10,11 @@ PDDLAction MacroGeneration::GenerateMacro(vector<PDDLActionInstance> actions) {
     for (PDDLActionInstance actionInstance : actions) {
         actionTypes.push_back(*actionInstance.action);
     }
-    PDDLAction macro = PDDLAction(GenerateName(actionTypes), GenerateParams(actions), GeneratePrecons(actions), GenerateEffs(actions));
+    auto names = GenerateName(actionTypes);
+    auto params = GenerateParams(actions);
+    auto precons = GeneratePrecons(actions);
+    auto effects = GenerateEffs(actions);
+    PDDLAction macro = PDDLAction(names, params, precons, effects);
     return macro;
 }
 
@@ -58,14 +62,14 @@ PDDLLiteral MacroGeneration::ConvertLiteral(PDDLLiteral lit, PDDLActionInstance 
     vector<unsigned int> newargs;
     for (unsigned int arg : lit.args) {
         // push the index of argument object to newargs
-        newargs.push_back(GetIndex(params, action.objects[arg]));
+        newargs.push_back(GetIndex(params, action.objects.at(arg)));
     }
     return PDDLLiteral(lit.predicateIndex, newargs, lit.value);
 }
 // good code
 vector<PDDLLiteral> MacroGeneration::RemoveFromPDDLVector(vector<PDDLLiteral> literals, set<int> removes) {
     vector<PDDLLiteral> newLiterals;
-    newLiterals.reserve(literals.size()-removes.size());
+    //newLiterals.reserve(literals.size()-removes.size());
     for (int i = 0; i < literals.size(); i++)
         if (!removes.contains(i))
             newLiterals.push_back(PDDLLiteral(literals.at(i)));
@@ -80,17 +84,17 @@ vector<PDDLLiteral> MacroGeneration::GeneratePrecons(vector<PDDLActionInstance> 
     for (int i = 0; i < size; ++i) {
         // because formula is pre(o1) U (pre(o2) \ eff+(o1)), a separate precons vector is needed
         vector<PDDLLiteral> additional_precons;
-        for (PDDLLiteral precon : actions[i].action->preconditions) {
-            additional_precons.push_back(ConvertLiteral(precon, actions[i], unique_parameters));
+        for (PDDLLiteral precon : actions.at(i).action->preconditions) {
+            additional_precons.push_back(ConvertLiteral(precon, actions.at(i), unique_parameters));
         }
         // remove positive effects of previous action from precons
         if (i != 0) {
-            vector<PDDLLiteral> previous_adds = actions[i-1].action->GetAdds();
+            vector<PDDLLiteral> previous_adds = actions.at(i-1).action->GetAdds();
             set<int> delete_list;
             // convert add literals to new ones
             // and add them to remove list if they exist in additional_precons
             for (PDDLLiteral add : previous_adds) {
-                PDDLLiteral converted_add = ConvertLiteral(add, actions[i-1], unique_parameters);
+                PDDLLiteral converted_add = ConvertLiteral(add, actions.at(i-1), unique_parameters);
                 // check if add exists in additional_precons, add it to delete_list
                 for (int j = 0; j < additional_precons.size(); ++j) {
                     PDDLLiteral* precon = &additional_precons.at(j);
@@ -125,23 +129,24 @@ vector<PDDLLiteral> MacroGeneration::GenerateEffs(vector<PDDLActionInstance> act
         vector<PDDLLiteral> o1_deletes;
 
         if (i == 0) {
-            o1_adds = actions[i].action->GetAdds();
-            o1_deletes = actions[i].action->GetDeletes();
+            o1_adds = actions.at(i).action->GetAdds();
+            o1_deletes = actions.at(i).action->GetDeletes();
         } else {
             o1_adds = preliminary_adds;
             o1_deletes = preliminary_deletes;
         }
 
-        vector<PDDLLiteral> o2_adds = actions[i+1].action->GetAdds();
-        vector<PDDLLiteral> o2_deletes = actions[i+1].action->GetDeletes();
+        vector<PDDLLiteral> o2_adds = actions.at(i+1).action->GetAdds();
+        vector<PDDLLiteral> o2_deletes = actions.at(i+1).action->GetDeletes();
 
         // adds(o1, o2) is (adds(o1) \ deletes(o2)) U adds(o2)
         // this is adds(o1) \ deletes(o2) part allegedly
-        for (int j = 0; o1_adds.size(); ++j) {
+        for (int j = 0; j < o1_adds.size(); ++j) {
             bool is_in_deletes = false;
-            PDDLLiteral converted_o1_add = i == 0 ? ConvertLiteral(o1_adds[j], actions[i], unique_parameters) : o1_adds[j];
+            // crash 💣
+            PDDLLiteral converted_o1_add = (i == 0) ? ConvertLiteral(o1_adds.at(j), actions.at(i), unique_parameters) : o1_adds.at(j);
             for (PDDLLiteral o2_delete : o2_deletes) {
-                PDDLLiteral converted_o2_delete = ConvertLiteral(o2_delete, actions[i+1], unique_parameters);
+                PDDLLiteral converted_o2_delete = ConvertLiteral(o2_delete, actions.at(i+1), unique_parameters);
                 if (converted_o1_add == converted_o2_delete) {
                     is_in_deletes = true;
                     break;
@@ -154,7 +159,7 @@ vector<PDDLLiteral> MacroGeneration::GenerateEffs(vector<PDDLActionInstance> act
         // U adds(o2) part
         for (PDDLLiteral o2_add : o2_adds) {
             bool is_in_adds = false;
-            PDDLLiteral converted_o2_add = ConvertLiteral(o2_add, actions[i+1], unique_parameters);
+            PDDLLiteral converted_o2_add = ConvertLiteral(o2_add, actions.at(i+1), unique_parameters);
             // check if add already exists in preliminary_adds, add it if it doesnt
             for (PDDLLiteral o1_add : preliminary_adds) {
                 if (converted_o2_add == o1_add) {
@@ -168,11 +173,11 @@ vector<PDDLLiteral> MacroGeneration::GenerateEffs(vector<PDDLActionInstance> act
         }
         // deletes(o1, o2) (deletes(o1) \ adds(o2)) U deletes(o2)
         // deletes(o1) \ adds(o2)
-        for (int j = 0; o1_deletes.size(); ++j) {
+        for (int j = 0; j < o1_deletes.size(); ++j) {
             bool is_in_adds = false;
-            PDDLLiteral converted_o1_delete = i == 0 ? ConvertLiteral(o1_deletes[j], actions[i], unique_parameters) : o1_deletes[j];
+            PDDLLiteral converted_o1_delete = (i == 0) ? ConvertLiteral(o1_deletes.at(j), actions.at(i), unique_parameters) : o1_deletes.at(j);
             for (PDDLLiteral o2_add : o2_adds) {
-                PDDLLiteral converted_o2_add = ConvertLiteral(o2_add, actions[i+1], unique_parameters);
+                PDDLLiteral converted_o2_add = ConvertLiteral(o2_add, actions.at(i+1), unique_parameters);
                 if (converted_o1_delete == converted_o2_add) {
                     is_in_adds = true;
                     break;
@@ -185,7 +190,7 @@ vector<PDDLLiteral> MacroGeneration::GenerateEffs(vector<PDDLActionInstance> act
         // U deletes(o2) part
         for (PDDLLiteral o2_delete : o2_deletes) {
             bool is_in_deletes = false;
-            PDDLLiteral converted_o2_delete = ConvertLiteral(o2_delete, actions[i+1], unique_parameters);
+            PDDLLiteral converted_o2_delete = ConvertLiteral(o2_delete, actions.at(i+1), unique_parameters);
             // check if add already exists in preliminary_adds, add it if it doesnt
             for (PDDLLiteral o1_delete : preliminary_adds) {
                 if (converted_o2_delete == o1_delete) {
@@ -197,7 +202,6 @@ vector<PDDLLiteral> MacroGeneration::GenerateEffs(vector<PDDLActionInstance> act
                 preliminary_deletes.push_back(converted_o2_delete);
             }
         }
-
         new_adds = preliminary_adds;
         new_deletes = preliminary_deletes;
     }
