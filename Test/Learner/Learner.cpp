eb849947d5adde2f100dd790e9fb2ce2fc05ba18
Christoffer Lind Andersen <chri692u@gmail.com>
2022-11-30 12:44:46 +0100

    Added start of Learner test

--- Test/Learner/Learner.cpp
+++ Test/Learner/Learner.cpp
@@ -6,29 +6,7 @@
 const std::string TAG = "Learner ";
 
 TEST_CASE(TAG + "Domain = gripper") {
-    std::vector<PDDLActionInstance*> actions;
-    // set up simple action with 1 precondition and 1 effect
-    PDDLAction _act = PDDLAction(
-        "testaction",
-        std::vector<std::string> {"?x", "?y"},
-        std::vector<PDDLLiteral> {PDDLLiteral(1, std::vector<unsigned int> {0}, true)},
-        std::vector<PDDLLiteral> {PDDLLiteral(2, std::vector<unsigned int> {1}, true)}
-    );
-    PDDLActionInstance act1 = PDDLActionInstance(&_act, std::vector<unsigned int> {0, 1});
-    PDDLActionInstance act2 = PDDLActionInstance(&_act, std::vector<unsigned int> {0, 1});
-    actions.push_back(&act1);
-    actions.push_back(&act2);
-    // generate macro
-    MacroGenerator macroGenerator = MacroGenerator();
-    Macro m = macroGenerator.GenerateMacro(&actions);
-    // expected stuff
-    std::unordered_map<GroundedLiteral, bool> expectedPrecons ({
-        {GroundedLiteral(1, std::vector<unsigned int> {0}), true}
-    });
-    std::unordered_map<GroundedLiteral, bool> expectedEffects ({
-        {GroundedLiteral(2, std::vector<unsigned int> {1}), true}
-    });
-    REQUIRE(m.groundedAction.parameters == std::unordered_set<unsigned int> {0, 1});
-    REQUIRE(m.groundedAction.preconditions == expectedPrecons);
-    REQUIRE(m.groundedAction.effects == expectedEffects);
+    Config config = Config();
+    PlanGenerator gen = PlanGenerator();
+    REQUIRE();
 }
