b8224dee1818475f3a343ba21bb971b614e417f6
Dovy <djasulaitis05@gmail.com>
2022-12-10 20:03:26 +0100

    add plan caching to plangenerator

--- src/PlanGenerator/PlanGenerator.cpp
+++ src/PlanGenerator/PlanGenerator.cpp
@@ -3,7 +3,6 @@
 #include <time.h>
 #include "PlanGenerator.hh"
 
-
 using namespace std;
 namespace fs = std::filesystem;
 
@@ -16,39 +15,70 @@ void PlanGenerator::GenerateSASPlans(Config config, string domainFile) {
     int NoTimeLimit = -1;
 
     string domainFolder = domainFile.substr(0, domainFile.find_last_of("\\/"));
+    string domainName = domainFolder.substr(domainFolder.find_last_of("/\\") + 1);
 
     srand(time(NULL));
     fs::path relativePath = fs::current_path();
+
     //Find path of x problems in the domain
     for (const auto & entry : fs::directory_iterator(domainFolder)){
         filePaths.push_back(relativePath / entry.path());
     }
+    if (!fs::is_directory(domainName + "Plans") || !fs::exists(domainName + "Plans")) {
+        cout << "didnt find plans, running downward" << endl;
+        fs::create_directory(domainName + "Plans");
+        unordered_set<int> attempts;
+        for(int i = 0; i < problemAmount && i < filePaths.size()-1; ++i){
+            int attempt = rand() % filePaths.size();
+            // least retarded c++ code
+            while (filePaths[attempt].substr(filePaths[attempt].find_last_of("/\\") + 1) == "domain.pddl" || attempts.contains(attempt)){
+                attempt = rand() % filePaths.size();
+            }
+            attempts.emplace(attempt);
+            PDDLDriver* driver = new PDDLDriver();
+            fs::path domainPath = relativePath / domainFile;
+            fs::path problemPath = relativePath / domainFolder / filePaths[attempt];
+            
+            driver->parse(domainPath);
+            driver->parse(problemPath);
 
-    //Read domain path and choose problemAmount to work with
-    for(int i = 0; i<problemAmount;++i){
-        int attempt = rand() % filePaths.size();
-        // least retarded c++ code
-        while (filePaths[attempt].substr(filePaths[attempt].find_last_of("/\\") + 1) == "domain.pddl"){
-            attempt = rand() % filePaths.size();
+            domains.push_back(PDDLConverter::Convert(driver->domain));
+            problems.push_back(PDDLConverter::Convert(&(domains.at(i)), driver->problem));
+
+            cout << "running downward for problem " << attempt << endl;
+            runner.RunDownward(config, domainPath, problemPath, NoTimeLimit);
+
+            //Save the plan
+            SASPlan plan = parser.Parse(relativePath / "sas_plan");
+            sasplans.push_back(plan);
+            // create plan file
+            ofstream ffs(domainName + "Plans/" + to_string(attempt));
+            ffs.close();
+            SASCodeGenerator sascodegen;
+            sascodegen.GenerateCode(plan, domainName + "Plans/" + to_string(attempt));
         }
-        cout << "hej haj" << endl;
-        PDDLDriver* driver = new PDDLDriver();
-        fs::path domainPath = relativePath / domainFile;
-        fs::path problemPath = relativePath / domainFolder / filePaths[attempt];
-        
-        driver->parse(domainPath);
-        driver->parse(problemPath);
+    } else {
+        cout << "found domain sas plans" << endl;
+        vector<int> attempts;
+        for (const auto & entry : fs::directory_iterator(domainName + "Plans")){
+            string path = entry.path();
+            attempts.push_back(stoi(path.substr(path.find_last_of("/\\") + 1)));
+        }
+        for (int i = 0; i < attempts.size(); ++i) {
+            int attempt = attempts.at(i);
+            PDDLDriver* driver = new PDDLDriver();
+            fs::path domainPath = relativePath / domainFile;
+            fs::path problemPath = relativePath / domainFolder / filePaths[attempt];
+            
+            driver->parse(domainPath);
+            driver->parse(problemPath);
 
-        domains.push_back(PDDLConverter::Convert(driver->domain));
-        problems.push_back(PDDLConverter::Convert(&(domains.at(i)), driver->problem));
+            domains.push_back(PDDLConverter::Convert(driver->domain));
+            problems.push_back(PDDLConverter::Convert(&(domains.at(i)), driver->problem));
 
-        //Run downward
-        runner.RunDownward(config, domainPath, problemPath, NoTimeLimit);
-
-        //Save the plan + pddl instance
-        SASPlan plan = parser.Parse(relativePath / "sas_plan");
-        sasplans.push_back(plan);
-
-        filePaths.erase(filePaths.begin()+attempt);   
+            //Save the plan
+            SASPlan plan = parser.Parse(relativePath / (domainName + "Plans") / to_string(attempt));
+            sasplans.push_back(plan);
+        }
     }
 }
